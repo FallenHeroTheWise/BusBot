450a2a7240484e055744704ff2cb1121bd3c6f72
FallenHeroTheWise <46132925+FallenHeroTheWise@users.noreply.github.com>
2023-06-12 21:53:49 +0400

    Add files via upload
    Added classes and stuff to make it more readable but it's probably less efficient

--- bot.py
+++ bot.py
@@ -1,182 +1,164 @@
-import telepot
-from telepot.loop import MessageLoop
-from pprint import pprint
-import random
-import time
-from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
-bot = telepot.Bot('5319724716:AAElD0JgkyeEWdGxW5g0Y0MQdBnlRO7BY_I')
-pompa={}
-answered=[]
-mustcheck=[]
-import requests
-from bs4 import BeautifulSoup
 import json
+import time
 from pprint import pprint
+import telepot
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from getapi import get_api
-def getUpdate():
-    
-    a=get_api()
-    jj=json.loads(a)
-    count=0
-    buses=[]
-    total=0
-    for y in jj['BUS']:
-        #that's the stupidest way to do it, past me, though I won't fix it
-        if True:
-            x=y['@attributes']
-            busid=x['BUS_ID']
-        
-            plate=x['PLATE']
-        
-            d=x['DRIVER_NAME']
-    
-            prevstop=x['PREV_STOP']
-        
-            current=x['CURRENT_STOP']
-        
-            speed=x['SPEED']
+from dataclasses import dataclass
 
-            model=x['BUS_MODEL']
+bot = telepot.Bot('5319724716:AAElD0JgkyeEWdGxW5g0Y0MQdBnlRO7BY_I')
+message_ids = {}
+answered = []
+must_check = []
 
-            lat=x['LATITUDE']
-    
-            lon=x['LONGITUDE']
-        
-            
-            name=x['ROUTE_NAME']
-            code=x['DISPLAY_ROUTE_CODE']
-            buses.append([busid, plate, d, prevstop, current, speed, model, lat, lon, name, code])
-    #returning a list of all the buses
-    return buses
 
-def sortbynumber(number):
-    
-    buses=getUpdate()
-    
-    for b in buses:
-        
-        if b[0]==number:
-            
-            return b
-        
-    #getting a certain bus by its unique id
-def updatebynumber(number, buses: list):
-    
-    for b in buses:
+class Buses:
+    def __init__(self, content=None):
+        if content is None:
+            content = []
+        self.content = content
 
-        if b[0]==number:
-            
-            return b
-        
-    #getting a certain bus by its unique id with list of buses as an argument 
-def sort(number, buses: list):
-    
-    pprint(len(buses))
-    
-    crrct=[]
-    
-    for b in buses:
-        
-        if b[-1]==number:
-            
-            crrct.append(b)
-            
-    #creating a list of matching buses
-            
-    if number!='85':
-        
-        routenames=[]
-        
-        for c in crrct:
-        
-            if not c[9] in routenames:
-                
-               routenames.append(c[9])
-               
-        sorted1=[]
-        
-        sorted2=[]
-        if len(routenames)==2:
-            
-            for c in crrct:
-                
-                if c[9]==routenames[0]:
-                    
-                    sorted1.append(c)
-                    
-                else:
-                    
-                    sorted2.append(c)
-                    
-        elif len(routenames)==1:
+    def __add__(self, other):
+        iter(other)
+        # check if object is iterable or not if not it will raise a TypeError
+        return self.content + other
 
-            
-
-            for c in crrct:
-
-                if c[9]==routenames[0]:
-
-                    sorted1.append(c)
-
-                    sorted2.append(c)
-
-                else:
-
-                    sorted2.append(c)
-
-                    sorted1.append(c)
-
-            routenames.append(routenames[0])
-            
-            #getting the routenames to send through telegram
-            
+    def __iadd__(self, other):
+        if type(other) == Bus:
+            self.content.append(other)
+            return self
         else:
-            
-            print(number)
-            
-            raise Exception
-        
-    if number=='85':
-        
-        routenames=[]
-        
+            iter(other)
+            # check if object is iterable or not if not it will raise a TypeError
+            self.content.extend(other)
+            return self
+
+    def __iter__(self):
+        return BusesIterator(self)
+
+    def __len__(self):
+        return len(self.content)
+
+    def __getitem__(self, item):
+        return self.content[item]
+
+
+class BusesIterator:
+    def __init__(self, bus_list):
+        self._bus_list = bus_list
+        self._index = 0
+
+    def __next__(self):
+        if self._index < len(self._bus_list.content):
+            self._index += 1
+            return self._bus_list.content[self._index - 1]
+        raise StopIteration
+
+
+@dataclass()
+class Bus:
+    bus_id: str
+    plate: str
+    driver_name: str
+    previous_stop: str
+    current_stop: str
+    speed: int
+    model: int
+    lat: int
+    lon: int
+    name: str
+    bus_code: str
+
+    def __iter__(self):
+        BusIterator(self)
+
+    def __getitem__(self, item):
+        return list(self.__dict__.values())[item]
+
+    def __setitem__(self, key, value):
+        self.__dict__.update({f'{list(self.__dict__.items())[key][0]}': value})
+
+
+class BusIterator:
+    def __init__(self, other):
+        self._index = 0
+        self._list = list(other.__dict__.keys())
+
+    def __next__(self):
+        if self._index < len(self._list):
+            self._index += 1
+            return self._list[self._index - 1]
+
+
+def get_update():
+    a = get_api()
+    json_grabbed = json.loads(a)
+    buses1 = Buses([])
+    for y in json_grabbed['BUS']:
+
+        x = y['@attributes']
+
+
+        buses1 += Bus(bus_id=x['BUS_ID'], plate=x["PLATE"], driver_name=x["DRIVER_NAME"], previous_stop=x['PREV_STOP'],
+                      current_stop=x["CURRENT_STOP"], speed=x['SPEED'], model=x['BUS_MODEL'], lat=x['LATITUDE'],
+                      lon=x['LONGITUDE'], name=x['ROUTE_NAME'], bus_code=x['DISPLAY_ROUTE_CODE'])
+    # returning a iterable and subscriptable object of all the buses
+    return buses1
+
+
+print(get_update()[1][-1])
+
+
+def updatebynumber(number, buses: Buses):
+    for b in buses:
+
+        if b[0] == number:
+            return b
+
+    # getting a certain bus by its unique id with list of buses as an argument
+
+
+def sort(number, buses: Buses):
+    pprint(len(buses))
+
+    crrct = []
+
+    for b in buses:
+
+        if b[-1] == number:
+            crrct.append(b)
+
+    # creating a list of matching buses
+
+    if number != '85':
+
+        routenames = []
+
         for c in crrct:
-            
-            c[9]=c[9].replace('Mərkəzi Bank', '28 Mall')
-        
+
             if not c[9] in routenames:
-                
-               routenames.append(c[9])
-               
-        sorted1=[]
-        
-        sorted2=[]
-        
-        if len(routenames)==2:
-            
+                routenames.append(c[9])
+
+        sorted1 = []
+
+        sorted2 = []
+        if len(routenames) == 2:
+
             for c in crrct:
-                
-                c[9]=c[9].replace('Mərkəzi Bank', '28 Mall')
-                
-                if c[9]==routenames[0]:
-                    
+
+                if c[9] == routenames[0]:
+
                     sorted1.append(c)
-                    
-                else:
-                    
-                    sorted2.append(c)
-                    
-            c[9]=c[9].replace('Mərkəzi Bank', '28 Mall')
-            
-        elif len(routenames)==1:
 
-            
+                else:
+
+                    sorted2.append(c)
+
+        elif len(routenames) == 1:
 
             for c in crrct:
-                
-                c[9]=c[9].replace('Mərkəzi Bank', '28 Mall')
-                
-                if c[9]==routenames[0]:
+
+                if c[9] == routenames[0]:
 
                     sorted1.append(c)
 
@@ -189,177 +171,225 @@ def sort(number, buses: list):
                     sorted1.append(c)
 
             routenames.append(routenames[0])
-            
+
+            # getting the routenames to send through telegram
+
+        else:
+
+            print(number)
+
+            raise Exception
+
+    if number == '85':
+
+        routenames = []
+
+        for c in crrct:
+
+            c[9] = c[9].replace('Mərkəzi Bank', '28 Mall')
+
+            if not c[9] in routenames:
+                routenames.append(c[9])
+
+        sorted1 = []
+
+        sorted2 = []
+
+        if len(routenames) == 2:
+
+            for c in crrct:
+
+                c[9] = c[9].replace('Mərkəzi Bank', '28 Mall')
+
+                if c[9] == routenames[0]:
+
+                    sorted1.append(c)
+
+                else:
+
+                    sorted2.append(c)
+
+            c[9] = c[9].replace('Mərkəzi Bank', '28 Mall')
+
+        elif len(routenames) == 1:
+
+            for c in crrct:
+
+                c[9] = c[9].replace('Mərkəzi Bank', '28 Mall')
+
+                if c[9] == routenames[0]:
+
+                    sorted1.append(c)
+
+                    sorted2.append(c)
+
+                else:
+
+                    sorted2.append(c)
+
+                    sorted1.append(c)
+
+            routenames.append(routenames[0])
+
     return [routenames, sorted1, sorted2]
 
+
 def handle(msg):
-    
     pprint(msg)
-    
-    global pompa
-    
-    if time.time()-msg['date']>300:
-        
-    	msg['text']=""
-    	
-    	print(time.time()-msg['date'])
-    	
-    text=msg['text']
-    
-    idd=msg['chat']['id']
-    
+
+    global message_ids
+
+    if time.time() - msg['date'] > 300:
+        msg['text'] = ""
+
+        print(time.time() - msg['date'])
+
+    text = msg['text']
+
+    idd = msg['chat']['id']
+
     if text.startswith('/bus '):
-        
+
         try:
-            
+
             bot.sendMessage(idd, 'Yoxlanılır')
-            
-            c=sort(text[4:].strip(), buses)
-            
-            pompa[idd]=c
-            
-            bot.sendMessage(idd, 'Hansı istiqamət', reply_markup=InlineKeyboardMarkup( inline_keyboard=[ [InlineKeyboardButton(text=c[0][0], callback_data='1'), InlineKeyboardButton(text=c[0][1], callback_data='2')]]))
+
+            c = sort(text[4:].strip(), buses1)
+
+            message_ids[idd] = c
+
+            bot.sendMessage(idd, 'Hansı istiqamət', reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text=c[0][0], callback_data='1'),
+                 InlineKeyboardButton(text=c[0][1], callback_data='2')]]))
 
         except:
-            
-            bot.sendMessage(idd, 'Yanlış istək')
-            
-    if text=='/bus':
-        
-        bot.sendMessage(idd, '/bus avtobus_nomresi')
-    
-def callbackQuery(query):
-    
-    global answered
-    
-    pprint(query)
-    
-    idd=query['message']['chat']['id']
-    
-    try:
-        
-        c=pompa[query['message']['chat']['id']]
-        
-    except KeyError:
-        
-        bot.sendMessage(idd, 'Köhnə mesaj')
-        
-        raise Exception
-    
-    if query['message']['message_id'] in answered:
 
+            bot.sendMessage(idd, 'Yanlış istək')
+
+    if text == '/bus':
+        bot.sendMessage(idd, '/bus avtobus_nomresi')
+
+
+def callback_query(query):
+    global answered
+
+    pprint(query)
+
+    idd = query['message']['chat']['id']
+
+    try:
+
+        c = message_ids[query['message']['chat']['id']]
+
+    except KeyError:
+
+        bot.sendMessage(idd, 'Köhnə mesaj')
+
+        raise Exception
+
+    if query['message']['message_id'] in answered:
         bot.answerCallbackQuery(query['id'], 'Artıq cavablandırılıb')
 
         raise Exception
 
     answered.append(query['message']['message_id'])
-    
+
     bot.answerCallbackQuery(query['id'], 'Qəbul edildi')
-    
+
     if query['data'].startswith('c'):
-        
-        d=updatebynumber(query['data'][1:], buses)
-        
+        d = updatebynumber(query['data'][1:], buses1)
+
         bot.sendLocation(idd, d[7].replace(',', '.'), d[8].replace(',', '.'),
-                             reply_markup=InlineKeyboardMarkup( inline_keyboard=[[
-                                 InlineKeyboardButton(text='Yenilə', callback_data='b'+d[1])]]))
-    
+                         reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
+                             InlineKeyboardButton(text='Yenilə', callback_data='b' + d[1])]]))
+
         bot.sendMessage(idd, 'Göndərildi')
     if query['data'].startswith('b'):
-        
-        d=updatebynumber(query['data'][1:], buses)
+        d = updatebynumber(query['data'][1:], buses1)
 
-        mass=bot.sendLocation(idd, d[7].replace(',', '.'), d[8].replace(',', '.'), live_period=1200)
+        mass = bot.sendLocation(idd, d[7].replace(',', '.'), d[8].replace(',', '.'), live_period=1200)
 
-        mustcheck.append([mass, time.time(), query['data'][1:]])
-    
+        must_check.append([mass, time.time(), query['data'][1:]])
+
         bot.sendMessage(idd, 'Göndərildi')
-    if query['data']=='1':
-        
-        for d in c[1]:
+    if query['data'] == '1':
 
+        for d in c[1]:
             print(d[7], d[8])
 
             bot.sendLocation(idd, d[7].replace(',', '.'), d[8].replace(',', '.'),
-                             reply_markup=InlineKeyboardMarkup( inline_keyboard=[[
-                                 InlineKeyboardButton(text='Yenilə', callback_data='b'+d[1])]]))
+                             reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
+                                 InlineKeyboardButton(text='Yenilə', callback_data='b' + d[1])]]))
         bot.sendMessage(idd, 'Göndərildi')
-    if query['data']=='2':
+    if query['data'] == '2':
 
         for d in c[2]:
-
             print(d[7], d[8])
 
-            bot.sendLocation(idd, d[7].replace(',', '.'), d[8].replace(',', '.'), reply_markup=InlineKeyboardMarkup( inline_keyboard=[[
-                                 InlineKeyboardButton(text='Yenilə', callback_data='b'+d[1])]]))
+            bot.sendLocation(idd, d[7].replace(',', '.'), d[8].replace(',', '.'),
+                             reply_markup=InlineKeyboardMarkup(inline_keyboard=[[
+                                 InlineKeyboardButton(text='Yenilə', callback_data='b' + d[1])]]))
 
         bot.sendMessage(idd, 'Göndərildi')
 
 
-
-
-
-def updateEverything(firsttime):
-
-    global buses
+def update_everything(first_time):
+    global buses1
 
     try:
 
-        buses=getUpdate()
+        buses1 = get_update()
 
-        if firsttime==0:
+        if first_time:
+            c = sort('85', buses1)
 
-            c=sort('85', buses)
-            
-            idd=685789544
-            
-            pompa[idd]=c
-                        
-            bot.sendMessage(685789544, 'Hansı istiqamət', reply_markup=InlineKeyboardMarkup( inline_keyboard=[ [InlineKeyboardButton(text=c[0][0], callback_data='1'), InlineKeyboardButton(text=c[0][1], callback_data='2')]]))
+            idd = 685789544
 
-            firsttime=1
-            
-    except:
-        
+            message_ids[idd] = c
+
+            bot.sendMessage(685789544, 'Hansı istiqamət', reply_markup=InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text=c[0][0], callback_data='1'),
+                 InlineKeyboardButton(text=c[0][1], callback_data='2')]]))
+
         pass
-    
-    for a in mustcheck:
-        
-            if time.time()-a[1]>1200:
-                
-                mustcheck.remove(a)
-                
-            else:
-                
-                try:
-                    
-                    d=updatebynumber(a[2], buses)
-                    
-                except:
-                    
-                    mustcheck.remove(a)
-                    
-                try:
-                    
-                    bot.editMessageLiveLocation((a[0]['chat']['id'], a[0]['message_id']), d[7].replace(',', '.'), d[8].replace(',', '.'))
+    except:
+        pass
 
-                except:
-                    
-                    print('fail')
-    
+    for a in must_check:
+
+        if time.time() - a[1] > 1200:
+
+            must_check.remove(a)
+
+        else:
+
+            try:
+
+                d = updatebynumber(a[2], buses1)
+
+            except:
+
+                must_check.remove(a)
+
+            try:
+
+                bot.editMessageLiveLocation((a[0]['chat']['id'], a[0]['message_id']), d[7].replace(',', '.'),
+                                            d[8].replace(',', '.'))
+
+            except:
+
+                print('fail')
+
 
 if __name__ == '__main__':
-    
-    bot.message_loop({'chat': handle, 'callback_query': callbackQuery})
 
-    firsttime = 0
+    bot.message_loop({'chat': handle, 'callback_query': callback_query})
 
-    updateEverything(firsttime)
+    first_time = 1
+
+    update_everything(first_time)
 
     while True:
-        
-        updateEverything(1)
+        update_everything(0)
 
         time.sleep(7)
-    
-    
